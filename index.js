5ad4a0ab2bed140a4c68677c702bd6bf5f5a1e83
Iqra <iqra.ali@student.moringaschool.com>
2023-03-17 00:13:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+var currentUser = 'IQRA ALI!';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser;
+
+var currentUser = "I";
+const shortGreeting = "Welcome, " + currentUser + '!';
